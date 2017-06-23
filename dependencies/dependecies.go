62cbd217ae154324a82c36d849a91d2a205a1db0
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-06-23 00:09:17 -0700

    feat: Add a way to setup dependencies

---
+++ dependencies/dependecies.go
@@ -0,0 +1,45 @@
+package dependencies
+
+import (
+	"github.com/Nivl/go-rest-tools/notifiers/mailer"
+	"github.com/bsphere/le_go"
+	"github.com/jmoiron/sqlx"
+)
+
+// DB represents an open connection with write access to the database
+var DB *sqlx.DB
+
+// InitPostgres inits the connection to the database
+func InitPostgres(uri string) error {
+	con, err := sqlx.Connect("postgres", uri)
+	if err != nil {
+		return err
+	}
+
+	// Unsafe returns a version of DB which will silently succeed to scan when
+	// columns in the SQL result have no fields in the destination struct.
+	DB = con.Unsafe()
+	return nil
+}
+
+// Logentries represents an open connection to logentries
+var Logentries *le_go.Logger
+
+// InitLogentries inits the connection to logentries
+func InitLogentries(token string) {
+	if token != "" {
+		le, err := le_go.Connect(token)
+		if err != nil {
+			panic(err)
+		}
+		Logentries = le
+	}
+}
+
+// Sendgrid is a sendgrid email client
+var Sendgrid *mailer.Sendgrid
+
+// InitSendgrid creates a mailer that uses Sendgrid
+func InitSendgrid(api, from, to string) {
+	Sendgrid = mailer.NewSendgrid(api, from, to)
+}
