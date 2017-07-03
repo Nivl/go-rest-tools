e169a13d9efe40aef56883ece72ee56c969b9777
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-07-03 11:32:40 -0700

    Feat: Make mailer.noop print results to stdout

--- notifiers/mailer/noop.go
+++ notifiers/mailer/noop.go
@@ -1,15 +1,21 @@
 package mailer
 
-// Noop is an object used to send email through sendgrid
+import (
+	"fmt"
+)
+
+// Noop is a mailer that just print emails
 type Noop struct {
 }
 
 // SendStackTrace emails the current stacktrace to the default FROM
 func (m *Noop) SendStackTrace(trace []byte, endpoint string, message string, id string) error {
+	fmt.Printf("%s\n%s\n%s", endpoint, message, trace)
 	return nil
 }
 
 // Send is used to send an email
 func (m *Noop) Send(msg *Message) error {
+	fmt.Printf("FROM: %s\nTO: %s\nSUBJECT: %s\n%s\n", msg.From, msg.To, msg.Subject, msg.Body)
 	return nil
 }
