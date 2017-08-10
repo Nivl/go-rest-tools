e6e5131a094656d511a72b46342f8d92538be178
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-08-10 00:22:13 -0700

    refactor(testauth): change the way data get created

--- security/auth/testauth/session.go
+++ security/auth/testauth/session.go
@@ -3,29 +3,27 @@ package testauth
 import (
 	"testing"
 
-	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 	"github.com/Nivl/go-rest-tools/security/auth"
 	"github.com/Nivl/go-rest-tools/storage/db"
+	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 )
 
 // NewAuth creates a new user and their session
 func NewAuth(t *testing.T, q db.DB) (*auth.User, *auth.Session) {
-	user := NewUser(t, q, nil)
-	session := &auth.Session{
-		UserID: user.ID,
-	}
-
-	if err := session.Create(q); err != nil {
-		t.Fatal(err)
-	}
-
-	lifecycle.SaveModels(t, session)
+	user := NewPersistedUser(t, q, nil)
+	session := NewPersistedSession(t, q, user)
 	return user, session
 }
 
 // NewAdminAuth creates a new admin and their session
 func NewAdminAuth(t *testing.T, q db.DB) (*auth.User, *auth.Session) {
-	user := NewUser(t, q, &auth.User{IsAdmin: true})
+	user := NewPersistedUser(t, q, &auth.User{IsAdmin: true})
+	session := NewPersistedSession(t, q, user)
+	return user, session
+}
+
+// NewSession creates a non-persisted session for the given user
+func NewSession(t *testing.T, q db.DB, user *auth.User) *auth.Session {
 	session := &auth.Session{
 		UserID: user.ID,
 	}
@@ -35,11 +33,11 @@ func NewAdminAuth(t *testing.T, q db.DB) (*auth.User, *auth.Session) {
 	}
 
 	lifecycle.SaveModels(t, session)
-	return user, session
+	return session
 }
 
-// NewSession creates a new session for the given user
-func NewSession(t *testing.T, q db.DB, user *auth.User) *auth.Session {
+// NewPersistedSession creates and persists a new session for the given user
+func NewPersistedSession(t *testing.T, q db.DB, user *auth.User) *auth.Session {
 	session := &auth.Session{
 		UserID: user.ID,
 	}
--- security/auth/testauth/user.go
+++ security/auth/testauth/user.go
@@ -4,20 +4,41 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
+	"github.com/satori/go.uuid"
+
 	"github.com/Nivl/go-rest-tools/security/auth"
 	"github.com/Nivl/go-rest-tools/storage/db"
+	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 	"github.com/dchest/uniuri"
 )
 
-// NewUser creates a new user with "fake" as password
-func NewUser(t *testing.T, q db.DB, u *auth.User) *auth.User {
+// NewUser returns a non-persisted user with "fake" as password
+func NewUser() *auth.User {
+	password, _ := auth.CryptPassword("fake")
+
+	return &auth.User{
+		ID:       uuid.NewV4().String(),
+		Name:     uniuri.New(),
+		Email:    fmt.Sprintf("%s@domain.tld", uniuri.New()),
+		Password: password,
+	}
+}
+
+// NewAdmin returns a non-persisted admin user with "fake" as password
+func NewAdmin() *auth.User {
+	user := NewUser()
+	user.IsAdmin = true
+	return user
+}
+
+// NewPersistedUser creates and persists a new user with "fake" as password
+func NewPersistedUser(t *testing.T, q db.DB, u *auth.User) *auth.User {
 	if u == nil {
 		u = &auth.User{}
 	}
 
 	if u.Email == "" {
-		u.Email = fmt.Sprintf("fake+%s@melvin.la", uniuri.New())
+		u.Email = fmt.Sprintf("fake+%s@domain.tld", uniuri.New())
 	}
 
 	if u.Name == "" {
