bfb7be44c5f139180f8dabc239915a318bdff748
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-08-31 23:20:33 -0700

    feat(mockdb): Add more expectation helpers + always use Once()

--- storage/db/mockdb/queryable_extension.go
+++ storage/db/mockdb/queryable_extension.go
@@ -38,21 +38,69 @@ func (mdb *Queryable) ExpectGet(typ string, runnable func(args mock.Arguments)) 
 	if runnable != nil {
 		getCall.Run(runnable)
 	}
-	return getCall
+	return getCall.Once()
+}
+
+// ExpectGetID is a helper that expects a Get with a specific ID
+func (mdb *Queryable) ExpectGetID(typ string, uuid string, runnable func(args mock.Arguments)) *mock.Call {
+	getCall := mdb.On("Get", mock.AnythingOfType(typ), StringType, uuid)
+	getCall.Return(nil)
+	if runnable != nil {
+		getCall.Run(runnable)
+	}
+	return getCall.Once()
 }
 
 // ExpectGetNotFound is a helper that expects a not found on a Get
 func (mdb *Queryable) ExpectGetNotFound(typ string) *mock.Call {
 	getCall := mdb.On("Get", mock.AnythingOfType(typ), StringType, StringType)
 	getCall.Return(sql.ErrNoRows)
-	return getCall
+	return getCall.Once()
+}
+
+// ExpectGetIDNotFound is a helper that expects a not found on a Get with a specific ID
+func (mdb *Queryable) ExpectGetIDNotFound(typ string, uuid string) *mock.Call {
+	getCall := mdb.On("Get", mock.AnythingOfType(typ), StringType, uuid)
+	getCall.Return(sql.ErrNoRows)
+	return getCall.Once()
+}
+
+// ExpectGetIDError is a helper that expects a connection error on a Get with a specific ID
+func (mdb *Queryable) ExpectGetIDError(typ string, uuid string) *mock.Call {
+	getCall := mdb.On("Get", mock.AnythingOfType(typ), StringType, uuid)
+	getCall.Return(serverError)
+	return getCall.Once()
+}
+
+// ExpectGetNoParams is a helper that expects a Get with no params but the stmt
+func (mdb *Queryable) ExpectGetNoParams(typ string, runnable func(args mock.Arguments)) *mock.Call {
+	call := mdb.On("Get", mock.AnythingOfType(typ), StringType)
+	call.Return(nil)
+	if runnable != nil {
+		call.Run(runnable)
+	}
+	return call.Once()
+}
+
+// ExpectGetNoParamsNotFound is a helper that expects a not found on a Get with no params but the stmt
+func (mdb *Queryable) ExpectGetNoParamsNotFound(typ string) *mock.Call {
+	call := mdb.On("Get", mock.AnythingOfType(typ), StringType)
+	call.Return(sql.ErrNoRows)
+	return call.Once()
+}
+
+// ExpectGetNoParamsError is a helper that expects a connection error on a Get with no params but the stmt
+func (mdb *Queryable) ExpectGetNoParamsError(typ string) *mock.Call {
+	call := mdb.On("Get", mock.AnythingOfType(typ), StringType)
+	call.Return(serverError)
+	return call.Once()
 }
 
 // ExpectGetError is a helper that expects a connection error on a Get
 func (mdb *Queryable) ExpectGetError(typ string) *mock.Call {
 	getCall := mdb.On("Get", mock.AnythingOfType(typ), StringType, StringType)
 	getCall.Return(serverError)
-	return getCall
+	return getCall.Once()
 }
 
 // ExpectSelect is an helper that expects a connection error on a Select
@@ -62,54 +110,54 @@ func (mdb *Queryable) ExpectSelect(typ string, runnable func(args mock.Arguments
 	if runnable != nil {
 		selectCall.Run(runnable)
 	}
-	return selectCall
+	return selectCall.Once()
 }
 
 // ExpectSelectError is an helper that expects a Select
 func (mdb *Queryable) ExpectSelectError(typ string) *mock.Call {
 	selectCall := mdb.On("Select", mock.AnythingOfType(typ), StringType, InType, InType)
 	selectCall.Return(serverError)
-	return selectCall
+	return selectCall.Once()
 }
 
 // ExpectDeletion is a helper that expects a deletion
 func (mdb *Queryable) ExpectDeletion() *mock.Call {
-	return mdb.On("Exec", StringType, StringType).Return(int64(1), nil)
+	return mdb.On("Exec", StringType, StringType).Return(int64(1), nil).Once()
 }
 
 // ExpectDeletionError is a helper that expects a deletion to fail
 func (mdb *Queryable) ExpectDeletionError() *mock.Call {
-	return mdb.On("Exec", StringType, StringType).Return(int64(0), serverError)
+	return mdb.On("Exec", StringType, StringType).Return(int64(0), serverError).Once()
 }
 
 // ExpectInsert is a helper that expects an insertion
 func (mdb *Queryable) ExpectInsert(typ string) *mock.Call {
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), nil)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), nil).Once()
 }
 
 // ExpectInsertError is a helper that expects an insert to fail
 func (mdb *Queryable) ExpectInsertError(typ string) *mock.Call {
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), serverError)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), serverError).Once()
 }
 
 // ExpectInsertConflict is a helper that expects a conflict on an insertion
 func (mdb *Queryable) ExpectInsertConflict(typ string, fieldName string) *mock.Call {
 	conflictError := newConflictError(fieldName)
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), conflictError)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), conflictError).Once()
 }
 
 // ExpectUpdate is a helper that expects an update
 func (mdb *Queryable) ExpectUpdate(typ string) *mock.Call {
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), nil)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(1), nil).Once()
 }
 
 // ExpectUpdateConflict is a helper that expects a conflict on an update
 func (mdb *Queryable) ExpectUpdateConflict(typ string, fieldName string) *mock.Call {
 	conflictError := newConflictError(fieldName)
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), conflictError)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), conflictError).Once()
 }
 
 // ExpectUpdateError is a helper that expects an update to fail
 func (mdb *Queryable) ExpectUpdateError(typ string) *mock.Call {
-	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), serverError)
+	return mdb.On("NamedExec", StringType, mock.AnythingOfType(typ)).Return(int64(0), serverError).Once()
 }
