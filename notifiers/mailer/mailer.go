09ee888f82b2b741451107fda5e62bd9f40e58cc
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-09-05 22:35:01 -0700

    Add more mock

--- notifiers/mailer/mailer.go
+++ notifiers/mailer/mailer.go
@@ -2,6 +2,9 @@ package mailer
 
 // Mailer is an object used to send email
 type Mailer interface {
+	// // SendStackTrace emails the current stacktrace to the default FROM
 	SendStackTrace(trace []byte, endpoint string, message string, id string) error
+
+	// Send is used to send an email
 	Send(msg *Message) error
 }
